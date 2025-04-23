f6fad9ea2d5f770cff4a81a229664314cb552bbd
scala <107001208+superlidada@users.noreply.github.com>
2025-04-23 22:21:23 +0800

    Update init-spark.sh

--- files/init-spark.sh
+++ files/init-spark.sh
@@ -4,6 +4,6 @@ set +ex
 
 sleep 10
 
-$SPARK_HOME/sbin/start-thriftserver.sh --packages io.delta:delta-core_2.11:0.6.1 --master "local[2]" | tee /tmp/output.txt
+$SPARK_HOME/sbin/start-thriftserver.sh --packages io.delta:delta-core_2.11:0.6.1 --master "local[2]" --hiveconf hive.server2.thrift.port=10001 | tee /tmp/output.txt
 
 /usr/bin/tail -f $(cat /tmp/output.txt | cut -d' ' -f 5)
